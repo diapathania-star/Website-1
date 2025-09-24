aa03ad626196b493e4433eed1e85458f4080e269
diapathania-star <diapathania@gmail.com>
2025-09-24 16:41:51 +0530

    Delete js/audit-intake.js

--- js/audit-intake.js
+++
@@ -1,7 +0,0 @@
-// Smooth-scroll to the form if URL has #form
-document.addEventListener('DOMContentLoaded', () => {
-  if (location.hash === '#form') {
-    const el = document.querySelector('.form-embed');
-    if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
-});
